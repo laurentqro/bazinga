687a0fe0259290b99a7657ccdba99f5378c4e759
Laurent Curau <laurent.curau@cafedelabourse.com>
2014-06-18 13:20:41 +0100

    Require bcrypt and mongoose + declare Mongoose schema for TV shows

--- server.js
+++ server.js
@@ -6,6 +6,34 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var showSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  airsDayOfWeek: String,
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  rating: Number,
+  ratingCount: Number,
+  status: String,
+  poster: String,
+  subscribers: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  }],
+  episodes: [{
+      season: Number,
+      episodeNumber: Number,
+      episodeName: String,
+      firstAired: Date,
+      overview: String
+  }]
+});
+
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());

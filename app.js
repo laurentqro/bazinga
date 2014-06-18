7287a12ae35b937e10510ae4f9ee780a4d9b4ff0
Laurent Curau <laurent.curau@cafedelabourse.com>
2014-06-18 12:49:01 +0100

    Enable HTML5 pushState

---
+++ app.js
@@ -0,0 +1,6 @@
+angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
+
+
+  }]);

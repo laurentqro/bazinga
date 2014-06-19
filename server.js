464877f665825e98a8e294a4993528d89ce33109
Laurent Curau <laurent.curau@cafedelabourse.com>
2014-06-19 18:18:44 +0100

    Create routes for signup/login/logout

--- server.js
+++ server.js
@@ -118,6 +118,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  res.cookie('user', JSON.stringify(req.user));
+  res.send(req.user);
+});
+
+app.post('/api/signup', function(req, res, next) {
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+  user.save(function(err) {
+    if (err) return next(err);
+    res.send(200);
+  });
+});
+
+app.get('/api/logout', function(req, res, next) {
+  req.logout();
+  res.send(200);
+});
+
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
   if (req.query.genre) {

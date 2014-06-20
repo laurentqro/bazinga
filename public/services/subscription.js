66b6ed372a16751e9648d0dbebd76d12385cea20
Laurent Curau <laurent.curau@cafedelabourse.com>
2014-06-20 18:26:51 +0100

    Remove user object function argument. We use req.user instead.

--- public/services/subscription.js
+++ public/services/subscription.js
@@ -1,10 +1,10 @@
 angular.module('MyApp')
   .factory('Subscription', ['$http', function($http) {
     return {
-      subscribe: function(show, user) {
+      subscribe: function(show) {
         return $http.post('/api/subscribe', { showId: show._id });
       },
-      unsubscribe: function(show, user) {
+      unsubscribe: function(show) {
         return $http.post('/api/unsubscribe', { showId: show._id });
       }
     };
